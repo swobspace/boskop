13a32de9386dcc819bbd55419c5aadb0422da5f6
Wolfgang Barth <wob@swobspace.net>
2014-09-05 18:45:40 +0200

    create wobauth/user.rb before calling generator

---
+++ app/models/wobauth/user.rb
@@ -0,0 +1,9 @@
+class Wobauth::User < ActiveRecord::Base
+  # dependencies within wobauth models
+  include Wobauth::Concerns::Models::User
+
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+	 :recoverable, :rememberable, :trackable
+end
