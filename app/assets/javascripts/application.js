08cb42c9c902f7a0d3004418622673d79dd714e7
Wolfgang Barth <wob@swobspace.net>
2014-09-06 13:20:00 +0200

    bootstrap, datatables and compagnion

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,4 +1,10 @@
+//
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require bootstrap
+//= require dataTables/jquery.dataTables
+//= require dataTables/bootstrap/3/jquery.dataTables.bootstrap
+//= require dataTables/extras/dataTables.tableTools
 //= require boskop
+//
