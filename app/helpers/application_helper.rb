0de706ef895eda7f133be8dcbc6ab11e26026a0b
Wolfgang Barth <wob@swobspace.net>
2014-09-06 17:17:54 +0200

    layout and navigation running

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,17 @@
 module ApplicationHelper
   include Wobapphelpers::Helpers::All
+
+    def configuration_active_class
+      if Boskop::CONFIGURATION_CONTROLLER.include?(controller.controller_name.to_sym)
+        "active"
+      end
+    end
+
+    def orgunits_active_class
+      if controller.controller_name.to_sym == :org_units
+        "active"
+      end
+    end
+
+
 end
