d7ab5bb5cf9b11a95e2d09a4b177a4bfa75331d1
Wolfgang Barth <wob@swobspace.net>
2014-09-07 12:07:06 +0200

    get location model spec running

--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Location, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to have_many(:merkmale) }
+
+  it { is_expected.to validate_presence_of(:name) }
+
+  it "should get plain factory working" do
+    f = FactoryGirl.create(:location)
+    g = FactoryGirl.create(:location)
+    expect(f).to be_valid
+    expect(g).to be_valid
+
+    is_expected.to validate_uniqueness_of :name
+  end
+
+  it "to_s returns value" do
+    f = FactoryGirl.create(:location, name: "MyName")
+    expect("#{f}").to be == "MyName"
+  end
+
 end
