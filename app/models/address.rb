e3c338c110fbdf5e652a7e565b7e7e1345e0a1ee
Wolfgang Barth <wob@swobspace.net>
2014-11-22 20:05:37 +0100

    set address location relation

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,12 @@
 class Address < ActiveRecord::Base
+  # -- associations
   belongs_to :addressfor, polymorphic: true
+
+  # -- configuration
+  # -- validations and callbacks
+
+
+  def to_s
+    "#{streetaddress}, #{plz} #{ort}"
+  end
 end
--- app/models/location.rb
+++ app/models/location.rb
@@ -1,12 +1,13 @@
 class Location < ActiveRecord::Base
   # -- associations
   has_many :merkmale, as: :merkmalfor, dependent: :destroy
+  has_many :addresses, as: :addressfor, dependent: :destroy
 
   # -- configuration
   has_ancestry :cache_depth =>true, :orphan_strategy => :adopt
   acts_as_list :scope => [:ancestry]
 
-  accepts_nested_attributes_for :merkmale, allow_destroy: true
+  accepts_nested_attributes_for :merkmale, :addresses,  allow_destroy: true
 
   # -- validations and callbacks
   validates :name, presence: true, uniqueness: true
