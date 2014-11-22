1a5c561f3aebbbafc8921111a281517e0535823a
Wolfgang Barth <wob@swobspace.net>
2014-11-22 18:08:25 +0100

    enforce mandantory and unique merkmalklassen

--- app/models/merkmal.rb
+++ app/models/merkmal.rb
@@ -6,10 +6,35 @@ class Merkmal < ActiveRecord::Base
   # -- configuration
   # -- validations and callbacks
 
-  validates :merkmalfor_id, :merkmalfor_type, :merkmalklasse_id, presence: true
+  validates :merkmalklasse_id, presence: true
+  validates :value, uniqueness: { if: :uniqueness_required? },
+                    presence:   { if: :is_mandantory? }
+
+  before_create :merkmalklasse_unique?
 
   def to_s
     "#{value.to_s}"
+  end
+
+  private
+
+  def uniqueness_required?
+    (!self.merkmalklasse.nil?) && self.merkmalklasse.unique
+  end
+
+  def is_mandantory?
+    (!self.merkmalklasse.nil?) && self.merkmalklasse.mandantory
+  end
+
+  def merkmalklasse_unique?
+    if Merkmal.where(merkmalfor_id: self.merkmalfor_id,
+                  merkmalfor_type: self.merkmalfor_type,
+                  merkmalklasse_id: self.merkmalklasse_id).empty?
+      true
+    else
+      errors.add(:base, 'only one attribute of each merkmalklasse allowed')
+      false
+    end
   end
 end
 
--- spec/models/merkmal_spec.rb
+++ spec/models/merkmal_spec.rb
@@ -1,11 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Merkmal, :type => :model do
+  let(:location)  { FactoryGirl.create(:location) }
+  let(:location2) { FactoryGirl.create(:location) }
+
   it { is_expected.to belong_to(:merkmalklasse) }
   it { is_expected.to belong_to(:merkmalfor) }
   it { is_expected.to validate_presence_of(:merkmalklasse_id) }
-  it { is_expected.to validate_presence_of(:merkmalfor_id) }
-  it { is_expected.to validate_presence_of(:merkmalfor_type) }
+  # it { is_expected.to validate_presence_of(:merkmalfor_id) }
+  # it { is_expected.to validate_presence_of(:merkmalfor_type) }
 
   it "should get plain factory working" do
     f = FactoryGirl.create(:merkmal)
@@ -17,6 +20,50 @@ RSpec.describe Merkmal, :type => :model do
   it "to_s returns value" do
     f = FactoryGirl.create(:merkmal, value: "MyName")
     expect("#{f}").to be == "MyName"
+  end
+
+  it "allows only one merkmal of the same merkmalklasse on the same object" do
+    mk = FactoryGirl.create(:merkmalklasse, for_object: "Location", unique: true)
+    f = FactoryGirl.create(:merkmal, merkmalklasse: mk, merkmalfor: location,
+                                     value: "Name1")
+    expect(f).to be_valid
+
+    expect {
+      FactoryGirl.create(:merkmal, merkmalklasse: mk, merkmalfor: location,
+                                   value: "Name2")
+    }.to raise_error(ActiveRecord::RecordNotSaved)
+  end
+
+  it "respects uniqueness of value" do
+    mk = FactoryGirl.create(:merkmalklasse, for_object: "Location", unique: true)
+    f = FactoryGirl.create(:merkmal, merkmalklasse: mk, merkmalfor: location,
+                                     value: "Name1")
+    g = FactoryGirl.build(:merkmal, merkmalklasse: mk, merkmalfor: location2,
+                                     value: "Name1")
+    expect(f).to be_valid
+    expect(g).to be_invalid
+  end
+
+  it "allows duplicate merkmale with same value if merkmalklasse is not uniq" do
+    mk = FactoryGirl.create(:merkmalklasse, for_object: "Location", unique: false)
+    f = FactoryGirl.create(:merkmal, merkmalklasse: mk, merkmalfor: location,
+                                     value: "Name1")
+    g = FactoryGirl.create(:merkmal, merkmalklasse: mk, merkmalfor: location2,
+                                     value: "Name1")
+    expect(f).to be_valid
+    expect(g).to be_valid
+  end
+
+  it "forces value if merkmalklasse is mandantory" do
+    mk = FactoryGirl.create(:merkmalklasse, for_object: "Location", mandantory: true)
+    f = FactoryGirl.build(:merkmal, merkmalklasse: mk, merkmalfor: location, value: nil)
+    expect(f).to be_invalid
+  end
+
+  it "allows empty value if merkmalklasse is optional" do
+    mk = FactoryGirl.create(:merkmalklasse, for_object: "Location", mandantory: false)
+    f = FactoryGirl.create(:merkmal, merkmalklasse: mk, merkmalfor: location, value: nil)
+    expect(f).to be_valid
   end
 
 end
