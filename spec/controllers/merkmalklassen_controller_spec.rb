a5df6cce6cd07aef6bcf79ff74bfa1f687e80bb4
Wolfgang Barth <wob@swobspace.net>
2014-11-23 16:56:21 +0100

    add login helper

--- spec/controllers/merkmalklassen_controller_spec.rb
+++ spec/controllers/merkmalklassen_controller_spec.rb
@@ -19,6 +19,7 @@ require 'rails_helper'
 # that an instance is receiving a specific message.
 
 RSpec.describe MerkmalklassenController, :type => :controller do
+  login_admin
 
   # This should return the minimal set of attributes required to create a valid
   # Merkmalklasse. As you add validations to Merkmalklasse, be sure to
