47db8ec56dc711df80ed691dfa223a5dba76d350
Wolfgang Barth <wob@swobspace.net>
2014-11-23 11:45:03 +0100

    set :cancan1 for wobapphelpers

--- config/initializers/wobapphelpers.rb
+++ config/initializers/wobapphelpers.rb
@@ -10,9 +10,10 @@ Wobapphelpers.setup do |config|
   #
   # show action links only if has ability to execute the action
   # * :none : always present action links
-  # * :cancan1 : for cancancan '~>1.7.0' or cancan '~>1.6.0'
+  # * :cancan1 : for cancancan '~>1.7' or cancan '~>1.6.0'
   # * :cancan2 : for cancan '~>2.0.0', untested
   #
   # default:
   # config.cancan = :none
+  config.cancan = :cancan1
 end
