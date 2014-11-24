e95019379d46c411885e2820dafac5e4a7046500
Wolfgang Barth <W.Barth@marienhaus-gmbh.de>
2014-11-24 09:20:43 +0100

    update import script

--- utils/import-locations.rb
+++ utils/import-locations.rb
@@ -14,6 +14,9 @@ unless (File.readable?("#{file}"))
   exit 1
 end
 
+merkmalklassen = Merkmalklasse.where(for_object: 'Location').map {|mk| [mk.name, mk.id]}
+merkmalklassen = Hash[*merkmalklassen.flatten]
+
 address_attributes = Address.attribute_names.reject! {|k,v| ['id', 'created_at', 'updated_at'].include?(k) }
 location_attributes = Location.attribute_names.reject! {|k,v| ['id', 'created_at', 'updated_at'].include?(k) }
 
@@ -23,9 +26,31 @@ CSV.foreach("#{file}", :col_sep =>";", :headers => true) do |row|
   next if use.blank?
   address_data  = hash.extract!(*address_attributes)
   location_data = hash.extract!(*location_attributes)
-   
-  puts location_data
-  puts address_data
-  puts hash
+  merkmal_data = hash.reject {|k,v| !merkmalklassen.keys.include?(k)}
+
+  puts "---"
+  puts "Location: #{location_data}"
+  puts "Adresse : #{address_data}"
+  puts "Merkmale: #{merkmal_data}"
+
+  location = Location.find_or_initialize_by(location_data)
+  if location.new_record?
+    location.addresses.build(address_data)
+    merkmal_data.each do |k,v|
+      location.merkmale.build(
+        merkmalklasse_id: merkmalklassen[k],
+        value: v
+      )
+    end
+    if location.save
+      puts "... saved"
+    else
+      puts "# ERROR, #{location.name} not saved"
+    end
+  else 
+    puts "- RECORD always exists"
+  end
+
+  break
 
 end
