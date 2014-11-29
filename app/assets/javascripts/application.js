c33546810ca339b97ec6f4fd77c5f4fa1b89efbb
Wolfgang Barth <wob@swobspace.net>
2014-11-29 11:24:21 +0100

    add dataTable colVis extension

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -6,5 +6,6 @@
 //= require dataTables/jquery.dataTables
 //= require dataTables/bootstrap/3/jquery.dataTables.bootstrap
 //= require dataTables/extras/dataTables.tableTools
+//= require dataTables/extras/dataTables.colVis
 //= require boskop
 //
