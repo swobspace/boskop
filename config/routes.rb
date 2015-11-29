2258f508b44c0cf3128096c830120852cb786205
Wolfgang Barth <wob@swobspace.net>
2015-11-29 18:15:34 +0100

    route root to locations#index as default

--- config/routes.rb
+++ config/routes.rb
@@ -20,7 +20,7 @@ Rails.application.routes.draw do
 
   mount Wobauth::Engine, at: '/auth'
 
-  root 'org_units#index'
+  root 'locations#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
