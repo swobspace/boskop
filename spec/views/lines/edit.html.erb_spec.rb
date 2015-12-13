2553c6bfbfd92c63f9f74a4a0ea2a59ff8e7bb05
Wolfgang Barth <wob@swobspace.net>
2015-12-13 19:35:39 +0100

    spec for lines views started, not finished yet

--- spec/views/lines/edit.html.erb_spec.rb
+++ spec/views/lines/edit.html.erb_spec.rb
@@ -2,6 +2,12 @@ require 'rails_helper'
 
 RSpec.describe "lines/edit", type: :view do
   before(:each) do
+    @ability = Object.new
+    @ability.extend(CanCan::Ability)
+    allow(controller).to receive(:current_ability) { @ability }
+    allow(controller).to receive(:controller_name) { "lines" }
+    allow(controller).to receive(:action_name) { "edit" }
+
     @line = assign(:line, Line.create!(
       :name => "MyString",
       :description => "MyText",
--- spec/views/lines/index.html.erb_spec.rb
+++ spec/views/lines/index.html.erb_spec.rb
@@ -2,6 +2,12 @@ require 'rails_helper'
 
 RSpec.describe "lines/index", type: :view do
   before(:each) do
+    @ability = Object.new
+    @ability.extend(CanCan::Ability)
+    allow(controller).to receive(:current_ability) { @ability }
+    allow(controller).to receive(:controller_name) { "lines" }
+    allow(controller).to receive(:action_name) { "edit" }
+
     assign(:lines, [
       Line.create!(
         :name => "Name",
