ecc0b476ebd38a2e0f78198baa303e135941b61a
Wolfgang Barth <Wolfgang.Barth@marienhaus.de>
2015-12-29 14:11:09 +0100

    compress output for down/upstream

--- spec/views/lines/index.html.erb_spec.rb
+++ spec/views/lines/index.html.erb_spec.rb
@@ -17,9 +17,9 @@ RSpec.describe "lines/index", type: :view do
         :location_a_id => 1,
         :location_b_id => 2,
         :access_type_id => 1,
-        :bw_upstream => "9.99",
+        :bw_upstream => "9.89",
         :bw_downstream => "19.99",
-        :bw2_upstream => "1.99",
+        :bw2_upstream => "1.54",
         :bw2_downstream => "4.99",
         :framework_contract => nil,
         :contract_period => 7,
@@ -37,9 +37,9 @@ RSpec.describe "lines/index", type: :view do
         :location_a_id => 1,
         :location_b_id => 2,
         :access_type_id => 1,
-        :bw_upstream => "9.99",
+        :bw_upstream => "9.89",
         :bw_downstream => "19.99",
-        :bw2_upstream => "1.99",
+        :bw2_upstream => "1.54",
         :bw2_downstream => "4.99",
         :framework_contract => nil,
         :contract_period => 7,
@@ -67,10 +67,8 @@ RSpec.describe "lines/index", type: :view do
     assert_select "tr>td", :text => "Provider".to_s, :count => 2
     assert_select "tr>td", :text => "Nirgendwo".to_s, :count => 2
     assert_select "tr>td", :text => "---".to_s, :count => 2
-    assert_select "tr>td", :text => "10.0".to_s, :count => 2
-    assert_select "tr>td", :text => "20.0".to_s, :count => 2
-    assert_select "tr>td", :text => "2.0".to_s, :count => 2
-    assert_select "tr>td", :text => "5.0".to_s, :count => 2
+    assert_select "tr>td", :text => "20.0 / 9.9".to_s, :count => 2
+    assert_select "tr>td", :text => "5.0 / 1.5".to_s, :count => 2
     assert_select "tr>td", :text => "myFrameworkContract".to_s, :count => 2
     assert_select "tr>td", :text => 7.to_s, :count => 2
     assert_select "tr>td", :text => "2 year".to_s, :count => 2
