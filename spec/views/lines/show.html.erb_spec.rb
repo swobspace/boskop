b77fb1edee77c1c1fb95a9dc96b753c377cab629
Wolfgang Barth <wob@swobspace.net>
2016-02-07 10:41:40 +0100

    link to locations and framework contract

--- spec/views/lines/show.html.erb_spec.rb
+++ spec/views/lines/show.html.erb_spec.rb
@@ -1,19 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe "lines/show", type: :view do
+  let(:user) { double }
+  let(:location) { FactoryGirl.create(:location, name: 'Nirgendwo') }
   before(:each) do
     @ability = Object.new
     @ability.extend(CanCan::Ability)
     allow(controller).to receive(:current_ability) { @ability }
     allow(controller).to receive(:controller_name) { "lines" }
     allow(controller).to receive(:action_name) { "show" }
+    allow(user).to receive(:can?).and_return(true)
+    allow(controller).to receive(:current_user) { user }
+
 
     @line = assign(:line, Line.create!(
       :name => "Name",
       :description => "MyDescription",
       :notes => "additional information",
       :provider_id => "Provider",
-      :location_a_id => 1,
+      :location_a_id => location.id,
       :location_b_id => nil,
       :access_type_id => 1,
       :bw_upstream => "9.99",
@@ -28,10 +33,9 @@ RSpec.describe "lines/show", type: :view do
       :renewal_unit => "year",
       :line_state_id => 1
     ))
-    expect(@line).to receive(:location_a).and_return("Nirgendwo")
-    expect(@line).to receive(:location_b).and_return("---")
+    expect(@line).to receive(:location_b).at_least(:once).and_return("---")
     expect(@line).to receive(:access_type).and_return("VDSL")
-    expect(@line).to receive(:framework_contract).and_return("myFrameworkContract")
+    expect(@line).to receive(:framework_contract).at_least(:once).and_return("myFrameworkContract")
     expect(@line).to receive(:line_state).and_return("active")
   end
 
@@ -41,7 +45,7 @@ RSpec.describe "lines/show", type: :view do
     expect(rendered).to match(/MyDescription/)
     expect(rendered).to match(/additional information/)
     expect(rendered).to match(/Provider/)
-    expect(rendered).to match(/Nirgendwo/)
+    expect(rendered).to match(/#{location.lid} \/ Nirgendwo \/  /)
     expect(rendered).to match(/---/)
     expect(rendered).to match(/VDSL/)
     expect(rendered).to match(/10.0/)
