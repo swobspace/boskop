9726c15bcfaa1ce42e33d22bc1b56e3f02fafce2
Wolfgang Barth <wob@swobspace.net>
2016-07-21 19:58:07 +0200

    update to devise 4.2

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -38,7 +38,7 @@ RSpec.configure do |config|
   config.infer_spec_type_from_file_location!
 
   # -- devise stuff
-  config.include Devise::TestHelpers, type: :controller
+  config.include Devise::Test::ControllerHelpers, type: :controller
   config.extend ControllerMacros, type: :controller
   config.include RequestMacros, type: :feature
 
