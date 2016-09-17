555d957f05707ed09f5c924861caf43353b5e837
Wolfgang Barth <wob@swobspace.net>
2016-09-17 12:28:48 +0200

    use capistrano/passenger for restart

--- config/deploy.rb
+++ config/deploy.rb
@@ -44,25 +44,6 @@ set :shell, "bash -l"
 # set :keep_releases, 5
 
 namespace :deploy do
-
-  desc 'Restart application'
-  task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
-      execute :touch, release_path.join('tmp/restart.txt')
-    end
-  end
-
-  after :publishing, :restart
-
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
-    end
-  end
-
   desc "Check that we can access everything"
   task :check_write_permissions do
     on roles(:all) do |host|
