dd7d74ef9b9b58e08f38865609ff1003ed4b850f
Wolfgang Barth <wob@swobspace.net>
2017-08-18 17:03:44 +0200

    remove Bower, use yarn now

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,22 +10,8 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 
-# >-----------------------------------------------------------------------
-# Bower related stuff
-# >-----------------------------------------------------------------------
-
-# Bower asset paths
-Rails.root.join('vendor', 'assets', 'bower_components').to_s.tap do |bower_path|
-  Rails.application.config.sass.load_paths << bower_path
-  Rails.application.config.assets.paths << bower_path
-end
-# Precompile Bootstrap fonts
-Rails.application.config.assets.precompile << %r(bootstrap-sass/assets/fonts/bootstrap/[\w-]+\.(?:eot|svg|ttf|woff2?)$)
-# Minimum Sass number precision required by bootstrap-sass
-::Sass::Script::Value::Number.precision = [8, ::Sass::Script::Value::Number.precision].max
-
-# Precompile datatables images
-Rails.application.config.assets.precompile += %w( datatables/media/images/* )
-
-# <--bower related -------------------------------------------------------
+# >>>> yarn
+Rails.application.config.assets.paths << Rails.root.join('node_modules')
+# Rails.application.config.assets.precompile << /\.(?:svg|eot|woff|woff2|ttf)\z/
+# <<<< yarn
 
