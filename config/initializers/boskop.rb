f47bc8887e4bc76d857f5a6f3990f5e0c3c1ca5f
Wolfgang Barth <wob@swobspace.net>
2017-08-19 19:34:32 +0200

    add hosts/host_categories to menu

--- config/initializers/boskop.rb
+++ config/initializers/boskop.rb
@@ -1,5 +1,5 @@
 module Boskop
-  CONFIGURATION_CONTROLLER = ['merkmalklassen', 'access_types', 'line_states', 'framework_contracts'].freeze
+  CONFIGURATION_CONTROLLER = ['merkmalklassen', 'access_types', 'line_states', 'framework_contracts', 'host_categories'].freeze
   PERIOD_UNITS = [ 'day', 'week', 'month', 'quarter', 'year' ].freeze
   
   def self.devise_modules
