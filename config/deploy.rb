29af95cfa97f4927abce2230e145bdea4ecc1d41
Wolfgang Barth <Wolfgang.Barth@marienhaus.de>
2017-08-21 09:52:16 +0200

    add node_modules to shared dirs

--- config/deploy.rb
+++ config/deploy.rb
@@ -34,7 +34,7 @@ set :linked_files, %w{config/database.yml config/application.yml config/secrets.
 
 # Default value for linked_dirs is []
 # set :linked_dirs, fetch(:linked_dirs, []).push('bin', 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
-set :linked_dirs, %w{bin log files tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs, %w{bin log files tmp/pids tmp/cache tmp/sockets vendor/bundle public/system node_modules}
 
 
 # Default value for default_env is {}
