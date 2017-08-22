5cb79d918a3f2a1f26f761e7add41745b7589519
Wolfgang Barth <Wolfgang.Barth@marienhaus.de>
2017-08-22 07:49:37 +0200

    update deploy.rb for capistrano 3.9

--- config/deploy.rb
+++ config/deploy.rb
@@ -30,11 +30,11 @@ set :deploy_to, config['deploy_to']
 # set :pty, true
 
 # Default value for :linked_files is []
-set :linked_files, %w{config/database.yml config/application.yml config/secrets.yml}
+append :linked_files, "config/database.yml", "config/application.yml", "config/secrets.yml"
 
 # Default value for linked_dirs is []
 # set :linked_dirs, fetch(:linked_dirs, []).push('bin', 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
-set :linked_dirs, %w{bin log files tmp/pids tmp/cache tmp/sockets vendor/bundle public/system node_modules}
+append :linked_dirs, "log", "files", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", "public/system", "node_modules"
 
 
 # Default value for default_env is {}
