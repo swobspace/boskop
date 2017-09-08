27e194282b36a0c5d921a153952eaf2379eed97f
Wolfgang Barth <wob@swobspace.net>
2017-09-08 17:53:49 +0200

    simple feature spec for operating_systems

--- spec/features/operating_systems_spec.rb
+++ spec/features/operating_systems_spec.rb
@@ -1,10 +1,11 @@
 require 'rails_helper'
 
-RSpec.describe "OperatingSystems", type: :request do
+RSpec.describe "OperatingSystems", type: :feature do
   describe "GET /operating_systems" do
-    it "works! (now write some real specs)" do
-      get operating_systems_path
-      expect(response).to have_http_status(200)
+    it "visits operating_systems#index" do
+      login_user
+      visit operating_systems_path
+      expect(current_path).to eq(operating_systems_path)
     end
   end
 end
