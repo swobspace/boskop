3e19ca7faa4f1356598841b903d50bfb583fccb0
Wolfgang Barth <wob@swobspace.net>
2017-09-16 13:17:15 +0200

    implement methods for merkmale
      given Merkmalklasse with :tag <tag>
      Host defines two new methods via method_missing:
        merkmal_<tag>  (getter)
        merkmal_<tag>= (setter)

--- app/models/host.rb
+++ app/models/host.rb
@@ -17,4 +17,38 @@ class Host < ApplicationRecord
     "#{ip} (#{name})"
   end
 
+private
+
+  def method_missing(key, *args)
+    case key
+    when *merkmal_attributes_getter
+      merkmale.where("merkmalklasse_id = :id", id: mk_id(key)).first&.value
+    when *merkmal_attributes_setter
+      merkmal = merkmale.find_or_initialize_by(merkmalklasse_id:  mk_id(key))
+      merkmal.value = args.first
+      if merkmal.save
+        merkmal.value
+      else
+        raise ArgumentError
+      end
+    else
+      super
+    end
+  end
+
+  def merkmal_attributes
+    Merkmalklasse.where(for_object: 'Host').pluck(:tag).map {|t| "merkmal_#{t}" }
+  end
+
+  def merkmal_attributes_getter
+    merkmal_attributes.map(&:to_sym)
+  end
+  def merkmal_attributes_setter
+    merkmal_attributes.map {|a| "#{a}=".to_sym }
+  end
+
+  def mk_id(rawkey)
+    key = rawkey.to_s.sub(/\Amerkmal_/, '').sub(/=\z/, '')
+    Merkmalklasse.where(for_object: 'Host', tag: key).first&.id
+  end
 end
--- spec/models/host_spec.rb
+++ spec/models/host_spec.rb
@@ -21,4 +21,37 @@ RSpec.describe Host, type: :model do
     expect("#{f}").to match ("192.0.2.77 (anyhost)")
   end
 
+  describe "Merkmale" do
+    let(:host) { FactoryGirl.create(:host) }
+    describe "#merkmal_doesnotexist" do
+      it { expect { host.merkmal_doesnotexist }.to raise_error(NoMethodError) }
+      it { expect { host.merkmal_doesnotexist=5 }.to raise_error(NoMethodError) }
+    end
+
+    describe "#merkmal_responsible" do
+      let!(:merkmalklasse) { FactoryGirl.create(:merkmalklasse,
+        name: "Responsible",
+        tag: 'responsible',
+        for_object: 'Host'
+      )}
+      it { expect(host.merkmal_responsible.nil?).to be_truthy }
+      it { expect(host.merkmal_responsible="Mr.X").to eq("Mr.X") }
+      it "assigns merkmal with value" do
+        host.merkmal_responsible = "Mr.X"
+        host.reload
+        expect(host.merkmale.where(merkmalklasse_id: merkmalklasse.id).
+               first.value).to eq("Mr.X")
+      end
+
+      context "with preassigned merkmal value" do
+        let!(:merkmal) {FactoryGirl.create(:merkmal, 
+          merkmalklasse_id: merkmalklasse.id,
+          merkmalfor: host,
+          value: "Hercule Poirot"
+        )}
+        it { expect(host.merkmal_responsible).to eq("Hercule Poirot") }
+      end
+    end
+  end
+
 end
