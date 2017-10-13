79ad3a3a180dbd3666b099099af01180e2a5bc77
Wolfgang Barth <wob@swobspace.net>
2017-10-13 18:57:54 +0200

    create OS-Mapping; started, not finished

--- app/jobs/create_operating_system_mappings_job.rb
+++ app/jobs/create_operating_system_mappings_job.rb
@@ -1,7 +1,18 @@
 class CreateOperatingSystemMappingsJob < ApplicationJob
   queue_as :default
 
-  def perform(*args)
-    # Do something later
+  def perform(options)
+    options.symbolize_keys! 
+    host = options.fetch(:host)
+    raise(ArgumentError, "not a host object") unless host.is_a? Host
+
+    [:cpe, :raw_os].each do |f|
+      value = host.send(f)
+      unless value.blank?
+        osm = OperatingSystemMapping.find_or_create_by(field: f.to_s, value: value)
+      end
+    end
+
+    
   end
 end
--- spec/jobs/create_operating_system_mappings_job_spec.rb
+++ spec/jobs/create_operating_system_mappings_job_spec.rb
@@ -1,5 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe CreateOperatingSystemMappingsJob, type: :job do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:win7) { FactoryGirl.create(:operating_system,
+    name: "Windows 7",
+    eol: "2020-01-14",
+    matching_pattern: "cpe:windows_7\nraw_os:Windows 7",
+)}
+  let(:host) { FactoryGirl.create(:host,
+    ip: '192.0.2.197',
+    raw_os: 'Windows 7 Professional',
+    cpe: "cpe:/o:microsoft:windows_7::sp1:professional",
+)}
+
+
+  describe "#perform" do
+    describe "without :host option" do
+      it "raises an error" do
+        expect {
+          CreateOperatingSystemMappingsJob.perform_now()
+        }.to raise_error(ArgumentError)
+      end
+    end
+    describe "with empty :host option" do
+      it "raises an error" do
+        expect {
+          CreateOperatingSystemMappingsJob.perform_now(host: nil)
+        }.to raise_error(ArgumentError)
+      end
+    end
+
+    describe "with new host" do
+      let(:job) { CreateOperatingSystemMappingsJob.perform_now(host: host) }
+  
+      it "creates an operating_system_mapping" do
+        expect { job }.to change(OperatingSystemMapping, :count).by(2)
+      end
+    end
+  end
+
 end
