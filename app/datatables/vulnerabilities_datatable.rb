ed850259914392e73c3a18fe2eef696cb56bfe45
Wolfgang Barth <wob@swobspace.net>
2017-10-22 13:22:55 +0200

    datatables object for vulnerabilities

---
+++ app/datatables/vulnerabilities_datatable.rb
@@ -0,0 +1,66 @@
+class VulnerabilitiesDatatable < ApplicationDatatable
+  delegate :edit_vulnerability_path, to: :@view
+  delegate :vulnerability_path, to: :@view
+
+  def initialize(relation, view)
+    @view = view
+    @relation = relation
+  end
+
+  private
+  attr_reader :relation
+
+  def data
+    vulnerabilities.map do |vuln|
+      [].tap do |column|
+        column << vuln.host.location.try(:lid)
+        column << vuln.host.ip.to_s
+        column << vuln.host.name
+        column << vuln.host.host_category.to_s
+        column << vuln.host.operating_system.to_s
+        column << vuln.vulnerability_detail.to_s
+        column << vuln.vulnerability_detail.threat
+        column << vuln.vulnerability_detail.severity.to_s
+        column << vuln.lastseen.to_s
+        links = []
+        links << show_link(vuln)
+        links << edit_link(vuln)
+        links << delete_link(vuln)
+        column << links.join('')
+      end
+    end
+  end
+
+  def count
+    Vulnerability.count
+  end
+
+  def total_entries
+    if params[:length] == "-1"
+      Vulnerability.count
+    else
+      vulnerabilities.total_count
+    end
+  end
+
+  def vulnerabilities
+    @vulnerabilities ||= fetch_vulnerabilities
+  end
+
+  def fetch_vulnerabilities
+    vulnerabilities = relation.order("#{sort_column} #{sort_direction}")
+    unless params[:length] == "-1"
+      vulnerabilities = vulnerabilities.page(page).per(per_page)
+    end
+    vulnerabilities = VulnerabilityQuery.new(vulnerabilities, search_params(params, search_columns)).all
+  end
+
+  def columns
+    %w(locations.lid host(hosts.ip) hosts.name host_categories.name operating_systems.name vulnerability_details.name vulnerabilities.threat vulnerabilities.severity vulnerabilities.lastseen)
+  end
+
+  def search_columns
+    %w(lid ip name hostname host_category operating_system vulnerability_detail threat severity lastseen)
+  end
+
+end
