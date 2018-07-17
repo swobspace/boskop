c970fa08e02cf1c3eef4f4f81bf12fcb14040275
Wolfgang Barth <Wolfgang.Barth@marienhaus.de>
2018-07-17 08:00:16 +0200

    fix file upload forms

--- spec/features/hosts_spec.rb
+++ spec/features/hosts_spec.rb
@@ -8,4 +8,21 @@ RSpec.describe "Hosts", type: :feature do
       expect(current_path).to eq(hosts_path)
     end
   end
+
+  describe "with import nmap xml file", js: false do
+    let(:nmapfile) { File.join(Rails.root, 'spec', 'fixtures', 'files', 'nmap-42.xml')}
+    it "uploads hosts" do
+      login_admin
+      visit hosts_path
+      click_link "Hosts aus Datei importieren"
+      within "form" do
+        select 'XML', :from => 'type'
+        select 'Newer', :from => 'update'
+        attach_file('file', nmapfile)
+        click_button "Upload"
+      end
+      expect(page).to have_content("Hosts")
+      expect(page).to have_content("Import successful")
+    end
+  end
 end
--- spec/features/vulnerabilities_spec.rb
+++ spec/features/vulnerabilities_spec.rb
@@ -8,4 +8,20 @@ RSpec.describe "Vulnerabilities", type: :feature do
       expect(current_path).to eq(vulnerabilities_path)
     end
   end
+
+  describe "with import nessus xml file", js: false do
+    let(:nessusfile) { File.join(Rails.root, 'spec', 'fixtures', 'files', 'cry-nessus.xml')}
+    it "uploads vulnerabilities" do
+      login_admin
+      visit vulnerabilities_path
+      click_link "Schwachstellenscan importieren"
+      within "form" do
+        select 'Nessus', :from => 'type'
+        attach_file('file', nessusfile)
+        click_button "Upload"
+      end
+      expect(page).to have_content("Schwachstellen")
+      expect(page).to have_content("Import successful")
+    end
+  end
 end
