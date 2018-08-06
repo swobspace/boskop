a65679f176fc7abe8171dda4777bcc4122da04ad
Wolfgang Barth <wob@swobspace.net>
2018-08-06 18:23:25 +0200

    eol_summary of hosts with pivottable.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,5 +18,6 @@
 //= require popper.js/dist/umd/popper
 //= require bootstrap/dist/js/bootstrap
 //= require jquery.dirtyforms/jquery.dirtyforms
+//= require pivottable/dist/pivot
 //= require boskop
 //
