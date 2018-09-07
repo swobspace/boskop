8e077d71133e3353c8ce53e54bd3dfddebe37f24
Wolfgang Barth <wob@swobspace.net>
2018-09-07 16:16:28 +0200

    simple feature spec

--- spec/features/nessus_scans_spec.rb
+++ spec/features/nessus_scans_spec.rb
@@ -1,10 +1,11 @@
 require 'rails_helper'
 
-RSpec.describe "NessusScans", type: :request do
+RSpec.describe "NessusScans", type: :feature do
   describe "GET /nessus_scans" do
-    it "works! (now write some real specs)" do
-      get nessus_scans_path
-      expect(response).to have_http_status(200)
+    it "displays index page" do
+      login_user
+      visit nessus_scans_path
+      expect(current_path).to eq(nessus_scans_path)
     end
   end
 end
