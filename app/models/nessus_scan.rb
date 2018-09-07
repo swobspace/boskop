4ddeb81a926ce5d80cb8ce02a8cedad6c47b474f
Wolfgang Barth <wob@swobspace.net>
2018-09-07 16:05:34 +0200

    complete model NessusScan

--- app/models/nessus_scan.rb
+++ app/models/nessus_scan.rb
@@ -1,2 +1,13 @@
 class NessusScan < ApplicationRecord
+  # -- associations
+
+  # -- configuration
+  # -- validations and callbacks
+  validates :uuid, presence: :true, uniqueness: true
+  validates :nessus_id, :name, :status, :last_modification_date, presence: :true
+
+  def to_s
+    "#{nessus_id} / #{name} / #{last_modification_date}"
+  end
+
 end
--- spec/models/nessus_scan_spec.rb
+++ spec/models/nessus_scan_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe NessusScan, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to validate_presence_of(:name) }
+  it { is_expected.to validate_presence_of(:uuid) }
+  it { is_expected.to validate_presence_of(:nessus_id) }
+  it { is_expected.to validate_presence_of(:status) }
+  it { is_expected.to validate_presence_of(:last_modification_date) }
+
+  it "should get plain factory working" do
+    f = FactoryBot.create(:nessus_scan)
+    g = FactoryBot.create(:nessus_scan)
+    expect(f).to validate_uniqueness_of(:uuid)
+    expect(f).to be_valid
+    expect(g).to be_valid
+  end
+
+  it "to_s returns value" do
+    f = FactoryBot.create(:nessus_scan, name: 'XYZ default', nessus_id: '1234',
+                          last_modification_date: Date.today)
+    expect("#{f}").to match ("1234 / XYZ default / #{Date.today.to_s}")
+  end
+
 end
