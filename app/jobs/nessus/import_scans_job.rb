d4d5330cac2aa2e7f8929caa5e5f0c48ad6c3daa
Wolfgang Barth <wob@swobspace.net>
2018-09-09 12:57:39 +0200

    add import_mode to #perform; improve logging and specs

--- app/jobs/nessus/import_scans_job.rb
+++ app/jobs/nessus/import_scans_job.rb
@@ -1,20 +1,40 @@
 class Nessus::ImportScansJob < ApplicationJob
   queue_as :default
 
-  def perform(*args)
+  def perform(options = {})
+    options.symbolize_keys!
+    @import_mode = options.fetch(:import_mode, 'auto')
     pending_scans.each do |scan|
       dlresult = DownloadNessusScanService.new(nessus_id: scan.nessus_id).call
-      imresult = ImportNessusVulnerabilitiesService.new(file: dlresult.xmlfile).call
-      if imresult.success?
-        scan.update_attributes(import_state: 'done')
+      if dlresult.success?
+        Rails.logger.info("Nessus XML #{dlresult.xmlfile} successfull downloaded")
+        imresult = ImportNessusVulnerabilitiesService.new(file: dlresult.xmlfile).call
+        if imresult.success?
+          Rails.logger.info("Nessus Scan successfully imported")
+          scan.update_attributes(import_state: 'done')
+        else
+        Rails.logger.warn("Import Nessus Scans failed for some reason: " +
+                          "#{imresult.error_message}")
+          scan.update_attributes(import_state: 'failed')
+        end
       else
+        Rails.logger.warn("Could not download Nessus XML file #{dlresult.xmlfile}: " +
+                          "#{dlresult.error_message}")
         scan.update_attributes(import_state: 'failed')
       end
     end
   end
 
 private
+  attr_reader :import_mode
   def pending_scans
-    NessusScan.where(import_state: 'new').limit(1)
+    importable_scans = NessusScan.where(import_state: 'new', status: 'completed',
+                                        import_mode: import_mode)
+    if importable_scans.any?
+      Rails.logger.info("Starting import on #{importable_scans.count} scan(s)")
+    else
+      Rails.logger.info("No pending scans, nothing to import")
+    end
+     importable_scans
   end
 end
--- spec/jobs/nessus/import_scans_job_spec.rb
+++ spec/jobs/nessus/import_scans_job_spec.rb
@@ -1,5 +1,51 @@
 require 'rails_helper'
 
+RSpec.shared_examples "a importable scan" do
+  it "doesn't raise an error" do
+    expect {
+      subject
+    }.not_to raise_error
+  end
+  it "creates an host" do
+    expect {
+      subject
+    }.to change{Host.count}.by(1)
+  end
+  it "creates 5 vulnerabilities" do
+    expect {
+      subject
+    }.to change(Vulnerability, :count).by(5)
+  end
+  it "creates 5 vulnerability_details" do
+    expect {
+      subject
+    }.to change{VulnerabilityDetail.count}.by(5)
+  end
+end
+
+RSpec.shared_examples "a non-importable scan" do
+  it "doesn't raise an error" do
+    expect {
+      subject
+    }.not_to raise_error
+  end
+  it "does not create an host" do
+    expect {
+      subject
+    }.to change{Host.count}.by(0)
+  end
+  it "does not create vulnerabilities" do
+    expect {
+      subject
+    }.to change(Vulnerability, :count).by(0)
+  end
+  it "does not create vulnerability_details" do
+    expect {
+      subject
+    }.to change{VulnerabilityDetail.count}.by(0)
+  end
+end
+
 RSpec.describe Nessus::ImportScansJob, type: :job do
   DLresult = ImmutableStruct.new( :success?, :error_message, :xmlfile )
   subject { Nessus::ImportScansJob.perform_now }
@@ -12,46 +58,82 @@ RSpec.describe Nessus::ImportScansJob, type: :job do
     DLresult.new(success: true, error_message: nil, xmlfile: xmlfile)
    }
 
-  let!(:nessus_scan) { FactoryBot.create(:nessus_scan,
+  let(:nessus_scan) { FactoryBot.create(:nessus_scan,
     uuid: "9608eba4-b33d-11e8-82aa-2387b3c5dd08",
     name: "XYZ default",
     nessus_id: "1234567",
-    status: "running",
     last_modification_date: 1.day.before(Date.today),
-    import_state: 'new'
   )}
   let(:xmlfile) { File.join(Rails.root, 'tmp', "#{nessus_scan.uuid}.nessus") }
 
   describe "#perform" do
-    before(:each) do
-      dlsvc = instance_double(DownloadNessusScanService)
-      expect(DownloadNessusScanService).to receive(:new).with(any_args).and_return(dlsvc)
-      expect(dlsvc).to receive(:call).and_return(dlresult)
+    describe "valid imports" do
+      before(:each) do
+       dlsvc = instance_double(DownloadNessusScanService)
+       expect(DownloadNessusScanService).to receive(:new).with(any_args).and_return(dlsvc)
+       expect(dlsvc).to receive(:call).and_return(dlresult)
+      end
+
+      describe "with import_mode: auto" do
+	before(:each) do
+	  nessus_scan.update_attributes(import_mode: 'auto', import_state: 'new', status: 'completed')
+	end
+	it_behaves_like "a importable scan"
+      end
+
+      describe "with import_mode: manual" do
+	subject { Nessus::ImportScansJob.perform_now(import_mode: 'manual') }
+	before(:each) do
+	  nessus_scan.update_attributes(import_mode: 'manual', import_state: 'new', status: 'completed')
+	end
+	it_behaves_like "a importable scan"
+      end
     end
 
-    it "doesn't raise an error" do
-      expect {
-        subject
-      }.not_to raise_error
-    end
+    describe "no imports on" do
+      describe "import_mode: unassigned, import_state: new" do
+	before(:each) do
+	  nessus_scan.update_attributes(import_mode: 'unassigned', import_state: 'new', status: 'completed')
+	end
+	it_behaves_like "a non-importable scan"
+      end
 
-    it "creates an host" do
-      expect {
-        subject
-      }.to change{Host.count}.by(1)
-    end
+      describe "import_mode: ignore, import_state: new" do
+	before(:each) do
+	  nessus_scan.update_attributes(import_mode: 'ignore', import_state: 'new', status: 'completed')
+	end
+	it_behaves_like "a non-importable scan"
+      end
 
-    it "creates 5 vulnerabilities" do
-      expect {
-        subject
-      }.to change(Vulnerability, :count).by(5)
-    end
+      describe "import_mode: auto, import_state: done" do
+	before(:each) do
+	  nessus_scan.update_attributes(import_mode: 'auto', import_state: 'done', status: 'completed')
+	end
+	it_behaves_like "a non-importable scan"
+      end
 
-    it "creates 5 vulnerability_details" do
-      expect {
-        subject
-      }.to change{VulnerabilityDetail.count}.by(5)
-    end
+      describe "import_mode: manual, import_state: done" do
+	subject { Nessus::ImportScansJob.perform_now(import_mode: 'manual') }
+	before(:each) do
+	  nessus_scan.update_attributes(import_mode: 'manual', import_state: 'done', status: 'completed')
+	end
+	it_behaves_like "a non-importable scan"
+      end
 
+      describe "import_mode: auto, status: running" do
+	before(:each) do
+	  nessus_scan.update_attributes(import_mode: 'auto', import_state: 'new', status: 'running')
+	end
+	it_behaves_like "a non-importable scan"
+      end
+
+      describe "import_mode: manual, status: running" do
+	subject { Nessus::ImportScansJob.perform_now(import_mode: 'manual') }
+	before(:each) do
+	  nessus_scan.update_attributes(import_mode: 'manual', import_state: 'new', status: 'running')
+	end
+	it_behaves_like "a non-importable scan"
+      end
+    end
   end
 end
