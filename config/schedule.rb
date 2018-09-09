e4e0a2575fc0f0f696aaa13c861cf04d9bd8dfcc
Wolfgang Barth <wob@swobspace.net>
2018-09-09 18:06:38 +0200

    fix typo

--- config/schedule.rb
+++ config/schedule.rb
@@ -5,11 +5,11 @@ env :https_proxy, nil
 
 set :output, "#{path}/log/whenever-boskop.log"
 
-every 1.day, at ['12:30', '19:30'] do
+every 1.day, at: ['12:30', '19:30'] do
   runner "Nessus::ListScansJob.perform_later"
 end
 
-every 1.day, at ['12:40', '18:10'] do
+every 1.day, at: ['12:40', '18:10'] do
   runner "Nessus::ImportScansJob.perform_later"
 end
 
