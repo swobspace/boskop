55c1a0b199c8188a35ce588723cc9696567ed723
Wolfgang Barth <wob@swobspace.net>
2018-09-10 14:34:04 +0200

    add some logging

--- app/jobs/nessus/list_scans_job.rb
+++ app/jobs/nessus/list_scans_job.rb
@@ -1,6 +1,5 @@
 require 'tenable-ruby'
 
-
 class Nessus::ListScansJob < ApplicationJob
   queue_as :default
 
@@ -17,25 +16,30 @@ class Nessus::ListScansJob < ApplicationJob
                url: Boskop.nessus_url
              )
     if nessus.authenticate
-      scan_list = nessus.scan_list
-      scan_list['scans'].each do |scan|
-        next if scan['uuid'].blank?	# no scan available
-        next if scan['name'] =~ /test/i # ignore test scans
-        nscan = NessusScan.find_or_initialize_by(nessus_id: scan['id'])
-        if nscan.import_mode.blank?
-          nscan.import_mode = 'unassigned'
-        end
-        nscan.name      = scan['name']
-        unless nscan.uuid == scan['uuid']
-          nscan.uuid         = scan['uuid']
-          nscan.import_state = 'new'
-        end
-        nscan.status    = scan['status']
-        nscan.last_modification_date = Time.at(scan['last_modification_date'].to_i)
-        nscan.save!
+      scan_list = nessus.scan_list['scans']
+      if scan_list.blank?
+         Rails.logger.warn "### Authenticate succeeded, but no data; check network, proxy settings and firewall"
+      else
+	scan_list.each do |scan|
+	  next if scan['uuid'].blank?	# no scan available
+	  next if scan['name'] =~ /test/i # ignore test scans
+	  nscan = NessusScan.find_or_initialize_by(nessus_id: scan['id'])
+	  if nscan.import_mode.blank?
+	    nscan.import_mode = 'unassigned'
+	  end
+	  nscan.name      = scan['name']
+	  unless nscan.uuid == scan['uuid']
+	    nscan.uuid         = scan['uuid']
+	    nscan.import_state = 'new'
+	  end
+	  nscan.status    = scan['status']
+	  nscan.last_modification_date = Time.at(scan['last_modification_date'].to_i)
+	  nscan.save!
+	end
+        Rails.logger.info "### #{scan_list.count} scans imported/updated"
       end
     else
-      raise RuntimeError, "could not authenticate, no scans listed"
+      Rails.logger.error "### Could not authenticate, no scans listed"
     end
   end
 end
