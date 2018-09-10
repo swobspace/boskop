341b6b1421edd05b006f8c16f6c454a82146da50
Wolfgang Barth <wob@swobspace.net>
2018-09-10 11:55:25 +0200

    remove path from schedule

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,15 +1,12 @@
-env :PATH, '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'
 env :RAILS_ENV, :production
-env :http_proxy, nil
-env :https_proxy, nil
 
 set :output, "#{path}/log/whenever-boskop.log"
 
 every 1.day, at: ['12:30', '19:30'] do
-  runner "Nessus::ListScansJob.perform_later"
+  runner "Nessus::ListScansJob.perform_now"
 end
 
 every 1.day, at: ['12:40', '18:10'] do
-  runner "Nessus::ImportScansJob.perform_later"
+  runner "Nessus::ImportScansJob.perform_now"
 end
 
