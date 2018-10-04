1c56d1bf3664c54c415e21fe04fade9156e8edf6
Wolfgang Barth <Wolfgang.Barth@marienhaus.de>
2018-10-04 11:36:34 +0200

    finish send vuln mail job

--- app/jobs/send_vulnerabilities_mail_job.rb
+++ app/jobs/send_vulnerabilities_mail_job.rb
@@ -1,25 +1,43 @@
 class SendVulnerabilitiesMailJob < ApplicationJob
   queue_as :default
 
-  def perform(options)
-    options.symbolize_keys!
-    since   = options.fetch(:since, Date.today)
-    mail_to = options.fetch(:mail_to)
+  #
+  # SendVulnerabilitiesMailJob.perform_now(options)
+  #
+  # possible options:
+  # * since: (date)
+  # * at: (date)
+  # * mail_to: (string or array) additional mail addresses
+  # * lid: (string) location shortcut
+  #
+  def perform(options = {})
+    @options = options.symbolize_keys!
+    mail_to = Array(options.fetch(:mail_to, []))
     lid     = options.fetch(:lid, false)
 
     if lid
       locations = Location.where(lid: lid).limit(1)
     else
-      raise RuntimeError, "not yet implemented"
-    # locations = Location.with_new_vulns_since(since)
+      locations = Location.with_new_vulns(since_or_at)
     end
 
     locations.each do |location|
+      recipients = Array(mail_to) + Array(location.vuln_responsible_mail)
       vulns = location.vulnerabilities.higher.current
       VulnerabilitiesMailer.
-        send_csv(vulnerabilities: vulns, mail_to: mail_to, lid: lid).
+        send_csv(vulnerabilities: vulns, mail_to: recipients, lid: location.lid).
         deliver_now
     end
-
   end
+
+private
+
+  def since_or_at
+    sat = @options.slice(:since, :at)
+    if sat.empty?
+      sat = { since: Date.today }
+    end
+    sat
+  end
+
 end
