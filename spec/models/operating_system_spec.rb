e447721abbc054a0ef2b1652b8021ec2f1531524
Wolfgang Barth <Wolfgang.Barth@marienhaus.de>
2018-10-17 14:09:30 +0200

    nullify OS in hosts and mappings, give no error

--- spec/models/operating_system_spec.rb
+++ spec/models/operating_system_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe OperatingSystem, type: :model do
-  it { is_expected.to have_many(:hosts) }
+  it { is_expected.to have_many(:hosts).dependent(:nullify) }
   it { is_expected.to have_many(:vulnerabilities).through(:hosts) }
-  it { is_expected.to have_many(:operating_system_mappings) }
+  it { is_expected.to have_many(:operating_system_mappings).dependent(:nullify) }
   it { is_expected.to validate_presence_of(:name) }
 
   it "should get plain factory working" do
