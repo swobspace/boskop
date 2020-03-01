980dc0d65c1de1ecd02472360218ce261ebd0349
Wolfgang Barth <wob@swobspace.net>
2020-03-01 19:15:43 +0100

    software_raw_data: views ready

--- spec/views/software_raw_data/index.html.erb_spec.rb
+++ spec/views/software_raw_data/index.html.erb_spec.rb
@@ -1,24 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe "software_raw_data/index", type: :view do
+  let(:sw) { FactoryBot.create(:software, name: "VeryCommon", vendor: "Universe Ltd.") }
   before(:each) do
+    @ability = Object.new
+    @ability.extend(CanCan::Ability)
+    allow(controller).to receive(:current_ability) { @ability }
+    allow(controller).to receive(:controller_name) { "software_raw_data" }
+    allow(controller).to receive(:action_name) { "index" }
+
     assign(:software_raw_data, [
       SoftwareRawDatum.create!(
-        software: nil,
-        name: "Name",
+        software_id: sw.id,
+        name: "VCommon1",
         version: "Version",
         vendor: "Vendor",
         count: 2,
         operating_system: "Operating System",
+        lastseen: Date.today,
         source: "Source"
       ),
       SoftwareRawDatum.create!(
-        software: nil,
-        name: "Name",
+        software_id: sw.id,
+        name: "VCommon2",
         version: "Version",
         vendor: "Vendor",
         count: 2,
         operating_system: "Operating System",
+        lastseen: Date.today,
         source: "Source"
       )
     ])
@@ -26,12 +35,14 @@ RSpec.describe "software_raw_data/index", type: :view do
 
   it "renders a list of software_raw_data" do
     render
-    assert_select "tr>td", text: nil.to_s, count: 2
-    assert_select "tr>td", text: "Name".to_s, count: 2
+    assert_select "tr>td", text: "VeryCommon (Universe Ltd.)".to_s, count: 2
+    assert_select "tr>td", text: "VCommon1".to_s, count: 1
+    assert_select "tr>td", text: "VCommon2".to_s, count: 1
     assert_select "tr>td", text: "Version".to_s, count: 2
     assert_select "tr>td", text: "Vendor".to_s, count: 2
     assert_select "tr>td", text: 2.to_s, count: 2
     assert_select "tr>td", text: "Operating System".to_s, count: 2
+    assert_select "tr>td", text: Date.today.to_s, count: 2
     assert_select "tr>td", text: "Source".to_s, count: 2
   end
 end
