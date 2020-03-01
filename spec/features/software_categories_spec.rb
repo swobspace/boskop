94e9526febf778f8a161f6276c5beca9f824dfe7
Wolfgang Barth <wob@swobspace.net>
2020-03-01 18:29:44 +0100

    update feature specs

--- spec/features/software_categories_spec.rb
+++ spec/features/software_categories_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe "SoftwareCategories", type: :feature do
   describe "GET /software_categories" do
-    it "visits org_units#index" do
+    it "visits software_categories#index" do
       login_user
       visit software_categories_path
       expect(current_path).to eq(software_categories_path)
