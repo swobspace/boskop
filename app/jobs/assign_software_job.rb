76f1e1555aec2c78d91be44ae42c80efe77850e8
Wolfgang Barth <Wolfgang.Barth@marienhaus.de>
2020-03-05 16:38:47 +0100

    assign software job running

--- app/jobs/assign_software_job.rb
+++ app/jobs/assign_software_job.rb
@@ -1,7 +1,16 @@
 class AssignSoftwareJob < ApplicationJob
   queue_as :default
 
-  def perform(*args)
-    # Do something later
+  def perform(options)
+    @options = options.symbolize_keys!
+    software_id = options.fetch(:software_id)
+
+    relation = SoftwareRawDatum.where(software_id: nil)
+    software_raw_data = SoftwareRawDataQuery.new(
+                          relation,
+                          software_id: software_id,
+                          use_pattern: true
+                        ).query
+    software_raw_data.update_all(software_id: software_id)
   end
 end
