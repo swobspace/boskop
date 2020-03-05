692f536a86cebe94f52a12cc2b17f47d45bbc5ab
Wolfgang Barth <Wolfgang.Barth@marienhaus.de>
2020-03-05 08:22:16 +0100

    using jsonb for software#pattern

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_29_183610) do
+ActiveRecord::Schema.define(version: 2020_03_05_071709) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -301,7 +301,7 @@ ActiveRecord::Schema.define(version: 2020_02_29_183610) do
 
   create_table "software", force: :cascade do |t|
     t.string "name", default: "", null: false
-    t.text "pattern"
+    t.jsonb "pattern", default: {}
     t.string "vendor", default: ""
     t.text "description"
     t.string "minimum_allowed_version", default: ""
