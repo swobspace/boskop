f551cc15d66324a1c75f71737b9fa596694c2d16
Wolfgang Barth <wob@swobspace.net>
2020-03-14 12:07:36 +0100

    add logger to assign software job

--- app/jobs/assign_software_job.rb
+++ app/jobs/assign_software_job.rb
@@ -16,11 +16,31 @@ class AssignSoftwareJob < ApplicationJob
                             software_id: sw_id,
                             use_pattern: true
                           ).query
+      mismatches = software_raw_data.where("software_id <> ?", sw_id)
+      if mismatches.any?
+        sw_name = Software.find(sw_id).name
+        logger.warn(
+                    "Mismatching entries for software id #{sw_id} [#{sw_name}]:" +
+                    " raw data always assigned to other software"
+                   )
+        mismatches.each do |mm|
+          logger.warn(
+                      "software_raw_data #{mm.id} always assigned to software" + 
+                      "#{mm.software_id} [#{mm.software}]"
+                     )
+        end
+      end
       unless force_update
         # update only raw data without assigned software
         software_raw_data = software_raw_data.where(software_id: nil)
       end
       software_raw_data.update_all(software_id: sw_id)
     end
+  end
+
+private
+
+  def logger
+    @logger ||= Logger.new(File.join(Rails.root, 'log', "assign_software_job_#{Rails.env}.log"))
   end
 end
