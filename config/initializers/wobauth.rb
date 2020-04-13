3cb5aa9bb88ddf15cd7db65e62049e764a76dea8
Wolfgang Barth <wob@swobspace.net>
2020-04-13 11:06:12 +0200

    remove ldap.yml, ldap_options goes to boskop.yml

--- config/initializers/wobauth.rb
+++ config/initializers/wobauth.rb
@@ -12,5 +12,5 @@ Wobauth.setup do |config|
   #
   # config.authorized_for_types = [ "MyClass", ...]
   #
-  config.ldap_config = File.join( Rails.root, 'config', 'ldap.yml')
+  config.ldap_config = File.join( Rails.root, 'config', 'boskop.yml')
 end
