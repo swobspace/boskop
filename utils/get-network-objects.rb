c478e50affbb1ab05b4c6358df37948de1a02824
Wolfgang Barth <Wolfgang.Barth@marienhaus.de>
2021-12-02 14:24:47 +0100

    filter some network addresses

--- utils/get-network-objects.rb
+++ utils/get-network-objects.rb
@@ -18,6 +18,14 @@ mknet_vlan = Merkmalklasse.where(for_object: 'Network', tag: 'vlan').ids.first
 mkloc_group = Merkmalklasse.where(for_object: 'Location', tag: 'standortgruppe').ids.first
 mkloc_type = Merkmalklasse.where(for_object: 'Location', tag: 'standorttyp').ids.first
 
+networks =  Network.joins(:location)
+            .where(locations: {disabled: false})
+            .where.not("netzwerk <<= '172.24.0.0/16'")
+            .where.not("netzwerk <<= '192.168.0.0/18'")
+            .where.not("netzwerk <<= '192.168.64.0/21'")
+            .where.not("netzwerk <<= '192.168.178.0/23'")
+            .where.not("netzwerk <<= '10.199.0.0/16'")
+
 CSV.open("#{file}", "wb", :col_sep =>"\t") do |csv|
  csv << [
           # tsystems stuff
@@ -41,7 +49,7 @@ CSV.open("#{file}", "wb", :col_sep =>"\t") do |csv|
           "Standorttyp",
           "Standortgruppe",
         ]
-  Network.joins(:location).where(locations: {disabled: false}).each do |net|
+   networks.each do |net|
    cidr = net.netzwerk.to_cidr_mask.to_i
    mask = (('1'*cidr)+('0'*(32-cidr))).scan(/.{8}/m).map{|e|e.to_i(2)}.join('.')
    vlan = net.merkmale.where(merkmalklasse_id: mknet_vlan).first
