ff351d8ab8b7a07c0a0dcae86486e1e0e1933391
Wolfgang Barth <wob@swobspace.net>
2023-01-13 17:06:21 +0100

    add more sort params

--- spec/datatables/vulnerabilities_spec.rb
+++ spec/datatables/vulnerabilities_spec.rb
@@ -75,7 +75,7 @@ RSpec.describe VulnerabilitiesDatatable, type: :model do
   describe "without search params, start:2, length:2" do
     let(:myparams) { ActiveSupport::HashWithIndifferentAccess.new(
       columns: {"0"=> {search: {value: ""}}},
-      order: {"0"=>{column: "1", dir: "asc"}},
+      order: {"0"=>{column: "1", dir: "asc"}, "1"=>{column: "9", dir: "asc"}},
       start: "2",
       length: "2",
       search: {value: "", regex: "false"}
