da1cb96f3aaa965a1de24f3331fe3a55c01aed08
Wolfgang Barth <wob@swobspace.net>
2025-01-24 16:25:59 +0100

    remove datatables responsivness

--- app/javascript/src/datatables-bs5.js
+++ app/javascript/src/datatables-bs5.js
@@ -11,7 +11,7 @@ pdfMake.addVirtualFileSystem(pdfFonts);
 
 import DataTable from 'datatables.net-bs5';
 import 'datatables.net-buttons-bs5';
-import 'datatables.net-responsive-bs5';
+// import 'datatables.net-responsive-bs5';
 import 'datatables.net-buttons/js/buttons.colVis.mjs';
 import 'datatables.net-buttons/js/buttons.html5.mjs';
 import 'datatables.net-buttons/js/buttons.print.mjs';
